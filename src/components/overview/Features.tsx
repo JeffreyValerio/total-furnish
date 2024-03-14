c511505a306425fd5a56daff64ec1328b83fe7a8
JeffreyValerio <jeffreyvalerio@hotmail.com>
2024-03-14 12:02:43 -0600

    STYLES

--- src/components/overview/Features.tsx
+++ src/components/overview/Features.tsx
@@ -33,7 +33,7 @@ export const FeaturesProducts = ({ products }: Props) => {
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {products?.map((product) => (
-                        product.type === 'BESTSELLER' && (
+                        product.type === 'FEATURE' && (
                             <React.Fragment key={product.id}>
                                 <ProductCard product={product ?? {}} />
                             </React.Fragment>
--- src/components/shared/Skeletons.tsx
+++ src/components/shared/Skeletons.tsx
@@ -1,7 +1,7 @@
 export const ProductCardSkeleton = () => {
     return (
         <div className="flex flex-col gap-4">
-            <div className="skeleton h-96 w-full"></div>
+            <div className="skeleton h-[300px] w-full"></div>
             <div className="skeleton h-4 w-full"></div>
             <div className="skeleton h-4 w-28"></div>
             {/* <div className="skeleton h-4 w-full"></div> */}
