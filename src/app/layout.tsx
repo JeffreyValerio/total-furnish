782afdf277d5749ab550e098c237d65405224535
JeffreyValerio <jeffreyvalerio@hotmail.com>
2024-03-14 12:41:04 -0600

    new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
-import { Footer, Header, Sidebar } from "@/components";
-import { poppins } from "@/config/fonts";
+import { montserrat } from "@/config/fonts";
 import Providers from "@/components/providers/Providers";
 
 export const metadata: Metadata = {
@@ -16,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="es" data-theme="light">
-      <body className={poppins.className}>
+      <body className={montserrat.className}>
         <Providers>
           {children}
         </Providers>
--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,4 +1,4 @@
 
-import { Poppins } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-export const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
+export const montserrat = Montserrat({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
